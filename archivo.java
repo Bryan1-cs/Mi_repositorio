a14ec3293e548f6cb17f85975eb9261a3c250e86
Brayan javier castro soza <brayan.castro670@std.uni.edu.ni>
2025-06-24 15:35:05 -0600

    mi primer cambio confirmado

---
+++ archivo.java
@@ -0,0 +1,14 @@
+public class archivo{
+
+    public static void main(String[] args) {
+        String nombre=estudiante;
+        int a = 5;
+        int b = 3;
+        int suma = a+b;
+
+        System.out.println("Hola "+ nombre+ "!");
+        System.out.println("La suma de "+ a+ "mas "+ b+ "es "+ suma);
+    }
+
+    
+}
